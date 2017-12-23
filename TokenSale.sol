cea99caf8fe87c994e772fc9a4fb6b196468c84c
heyryanleys <ryanleys@gmail.com>
2017-12-23 15:11:38 -0500

    Init commit

---
+++ TokenSale.sol
@@ -0,0 +1,89 @@
+pragma solidity ^0.4.16;
+
+interface token {
+  function transfer(address receiver, uint amount);
+}
+
+contract Crowdsale {
+  address public beneficiary;
+  uint public fundingGoal;
+  uint public amountRaised;
+  uint public deadline;
+  uint public price;
+  token public tokenReward;
+  mapping(address => uint256) public balanceOf;
+  bool fundingGoalReached = true;
+  bool crowdsaleClosed = false;
+
+  event GoalReached(address recipient, uint totalAmountRaised);
+  event FundTransfer(address backer, uint amount, bool isContribution);
+
+  /**
+   * Constrctor function
+   *
+   * Setup the owner
+   */
+  function Crowdsale(
+    address ifSuccessfulSendTo,
+    uint fundingGoalInEthers,
+    uint durationInMinutes,
+    uint etherCostOfEachToken,
+    address addressOfTokenUsedAsReward
+  ) {
+    beneficiary = ifSuccessfulSendTo;
+    fundingGoal = fundingGoalInEthers * 1 ether;
+    deadline = now + durationInMinutes * 1 minutes;
+    price = etherCostOfEachToken * 1 ether;
+    tokenReward = token(addressOfTokenUsedAsReward);
+  }
+
+  /**
+   * Default function
+   *
+   * The function without name is the default function that is called whenever anyone sends funds to a contract
+   */
+  function () payable {
+    require(!crowdsaleClosed);
+    uint amount = msg.value;
+    balanceOf[msg.sender] += amount;
+    amountRaised += amount;
+    tokenReward.transfer(msg.sender, amount / price);
+    FundTransfer(msg.sender, amount, true);
+    msg.sender.send(amount);//send whatever eth are sent to the contract straight back to the contributor
+  }
+
+  modifier afterDeadline() { if (now >= deadline) _; }
+
+  /**
+   * Check if goal was reached
+   *
+   * Checks if the goal or time limit has been reached and ends the campaign
+   */
+  function checkGoalReached() afterDeadline {
+    if (amountRaised >= fundingGoal){
+        fundingGoalReached = true;
+        GoalReached(beneficiary, amountRaised);
+    }
+    crowdsaleClosed = true;
+  }
+
+
+  /**
+   * Withdraw the funds
+   *
+   * Checks to see if goal or time limit has been reached, and if so, and the funding goal was reached,
+   * sends the entire amount to the beneficiary. If goal was not reached, each contributor can withdraw
+   * the amount they contributed.
+   */
+  function safeWithdrawal() afterDeadline {
+    uint amount = balanceOf[msg.sender];
+    balanceOf[msg.sender] = 0;
+    if (amount > 0) {
+        if (msg.sender.send(amount)) {
+            FundTransfer(msg.sender, amount, false);
+        } else {
+            balanceOf[msg.sender] = amount;
+        }
+    }
+  }
+}
